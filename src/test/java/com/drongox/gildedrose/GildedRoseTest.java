2358f7d151e441e4cd3995dedda2801469f37db9
Hamza Saoudi <hamza.saoudi@renault.com>
2019-06-06 14:47:34 +0200

    add two tests

--- src/test/java/com/drongox/gildedrose/GildedRoseTest.java
+++ src/test/java/com/drongox/gildedrose/GildedRoseTest.java
@@ -3,16 +3,47 @@
  */
 package com.drongox.gildedrose;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 
 public class GildedRoseTest
 {
   @Test
-  public void testOmarIsInTheRoom()
+  public void should_decrease_by_one_day_given_standard_item()
   {
-    throw new OutOfMemoryError("Omar c'est ta faute!!!!!!");
+    //given
+    String expected = "milk, 3, 14";
+    Item[] items = new Item[]{
+        new Item("milk",4,15)
+    };
+
+    GildedRose gildedRose = new GildedRose(items);
+
+    //when
+    gildedRose.updateQuality();
+
+    //then
+    Assertions.assertThat(gildedRose.items[0].toString()).isEqualTo(expected);
   }
+
+  @Test
+  public void should_decrease_quality_by_two_when_the_sellin_passed()
+  {
+    //given
+    String expected = "milk, -2, 13";
+    Item[] items = new Item[]{
+        new Item("milk",-1,15)
+    };
+
+    GildedRose gildedRose = new GildedRose(items);
+
+    //when
+    gildedRose.updateQuality();
+
+    //then
+    Assertions.assertThat(gildedRose.items[0].toString()).isEqualTo(expected);
+  }
+
+
 }
